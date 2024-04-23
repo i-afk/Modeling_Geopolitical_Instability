4e61dd335232766516aefa25851a036fbd59d530
Ian <62959817+i-afk@users.noreply.github.com>
2024-04-23 13:34:43 -0400

    Predicting Changes in State Fragility

--- .ipynb_checkpoints/Predicting Change in Instability-checkpoint.R
+++ .ipynb_checkpoints/Predicting Change in Instability-checkpoint.R
@@ -1,4 +1,6 @@
-# Predicting Change in Instability 
+################### Predicting Change in Instability #################################
+# Can we predict future  changes in State Fragility using the current state of the
+# Fragile State Index (FSI)? 
 
 # Setwd
 setwd("C:/Users/Ian/Desktop/Geo Instability/FSI")
@@ -10,7 +12,11 @@ library(dplyr)
 #install.packages("lubridate")
 library(lubridate)
 
+# Set the directory where your Excel files are located
+#excel_folder = "C:/Users/Ian/Desktop/Geo Instability/FSI"
 
+# List all Excel files in the folder
+#excel_files = list.files(excel_folder, pattern = "\\.xlsx$", full.names = TRUE)
 
 # Importing Data
 d23 = read_excel("fsi-2023.xlsx")
@@ -40,6 +46,9 @@ list1 = list(c(d22, d21, d20, d19, d18, d17, d16, d15, d14, d13, d12, d11, d10, 
 # "Change from Previous year"
 d19 = d19[,!(names(d19) %in% "Change from Previous Year")]
 d20 = d20[,!(names(d20) %in% "Change from Previous Year")]
+
+
+# build a function which creates a new column for difference between the two years
 
 
 # Convert the Year column in d21 and d23 to datetime
@@ -91,11 +100,60 @@ d06 = inner_join(d06,n07, by = 'Country')
 
 # append columns 
 d23$Total.y = NA
+
 # rename Total to fit
 d23 = d23 %>%
                   rename(Total.x = Total)
 
 d06_23 = bind_rows(d23, d22, d21, d20, d19, d18, d17, d16, d15, d14, d13, d12, d11, d10, d09, d08, d07, d06)
 
-# export csv
+# rename Total to fit
+d06_23 = d06_23 %>%
+                  rename(Total = Total.x)
+d06_23 = d06_23 %>%
+                                 rename(Total_nxt = Total.y)
+
+
+# Export csv
 write.csv(d06_23, file ="/Users/Ian/Desktop/Geo Instability/d06_23.csv", row.names=FALSE)
+#write.csv(d23, file ="/Users/Ian/Desktop/Geo Instability/FSI/d23.csv", row.names=FALSE)
+
+
+
+# We only need 06-22 for regression, can be used later on 23
+d06_22 = bind_rows(d22, d21, d20, d19, d18, d17, d16, d15, d14, d13, d12, d11, d10, d09, d08, d07, d06)
+
+# rename Total to fit
+d06_22 = d06_22 %>%
+                  rename(Total = Total.x)
+d06_22 = d06_22 %>%
+                  rename(Total_nxt = Total.y)
+
+# Create Total Change Column 
+d06_22$Total_Change = d06_22$Total_nxt - d06_22$Total 
+
+
+#############################################################################
+# v1. will only include the FSI data 
+# v2. will include interaction terms and dummy variables
+                  # Consider adding in a previous year column
+                  # 
+#############################################################################
+
+
+############################# v1. ###########################################
+
+# v1. doesn't need country year or rank 
+df = d06_22[,!(names(d06_22) %in% c("Country", "Year", "Rank", "Total_nxt"))] # Do we want current instability total in the regression?
+
+# Initial Exploratory Regression on Total Change 
+ex_model = glm(df$Total_Change ~ df$Total + df$`C1: Security Apparatus`+df$`C2: Factionalized Elites`+df$`C3: Group Grievance`+df$`E1: Economy`
+               + df$`E2: Economic Inequality`+ df$`E3: Human Flight and Brain Drain`+df$`P1: State Legitimacy`+df$`P2: Public Services`+df$`P3: Human Rights`
+               + df$`S1: Demographic Pressures`+ df$`S2: Refugees and IDPs`+ df$`X1: External Intervention`)
+
+summary(ex_model)
+
+
+# We can assess the change in the sum total of 
+
+
